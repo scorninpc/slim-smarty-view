b7cc8aba3fa2521cfa1fcafbebf2607502c57041
Bruno Pitteli Gonçalves <scorninpc@gmail.com>
2026-04-10 00:09:30 -0300

    formating, PR for 8.4 compatible

--- src/Smarty.php
+++ src/Smarty.php
@@ -14,198 +14,202 @@ use SmartyException;
  */
 class Smarty implements \ArrayAccess
 {
-    /**
-     * Smarty instance
-     *
-     * @var \Smarty
-     */
-    protected $smarty;
+	/**
+	 * Smarty instance
+	 *
+	 * @var \Smarty
+	 */
+	protected $smarty;
 
-    /**
-     * Default view variables
-     *
-     * @var array
-     */
-    protected $defaultVariables = [];
+	/**
+	 * Default view variables
+	 *
+	 * @var array
+	 */
+	protected $defaultVariables = [];
 
-    /********************************************************************************
-     * Constructors and service provider registration
-     *******************************************************************************/
+	/********************************************************************************
+	 * Constructors and service provider registration
+	 *******************************************************************************/
 
-    /**
-     * Create new Smarty view
-     *
-     * @param string|array $paths Paths to templates directories
-     * @param array $settings Smarty settings
-     */
-    public function __construct($options = [])
-    {
-        $this->smarty = new \Smarty\Smarty();
+	/**
+	 * Create new Smarty view
+	 *
+	 * @param string|array $paths Paths to templates directories
+	 * @param array $settings Smarty settings
+	 */
+	public function __construct($options = [])
+	{
+		$this->smarty = new \Smarty\Smarty();
 
-        $this->smarty->setForceCompile($options['force_compile']);
-        $this->smarty->setDebugging($options['debugging']);
+		$this->smarty->setForceCompile($options['force_compile']);
+		$this->smarty->setDebugging($options['debugging']);
 
-        if($options['compile_check']) {
-            $this->smarty->setCompileCheck(\Smarty\Smarty::COMPILECHECK_ON);
-        }
-        else {
-            $this->smarty->setCompileCheck(\Smarty\Smarty::COMPILECHECK_OFF);
-        }
-        
-        $this->smarty->setCacheDir($options['cache_dir']);
-        $this->smarty->setCaching($options['caching']);
-        $this->smarty->setCacheLifetime($options['cache_lifetime']);
+		if($options['compile_check']) {
+			$this->smarty->setCompileCheck(\Smarty\Smarty::COMPILECHECK_ON);
+		}
+		else {
+			$this->smarty->setCompileCheck(\Smarty\Smarty::COMPILECHECK_OFF);
+		}
+		
+		$this->smarty->setCacheDir($options['cache_dir']);
+		$this->smarty->setCaching($options['caching']);
+		$this->smarty->setCacheLifetime($options['cache_lifetime']);
 
-        $this->smarty->setTemplateDir($options['template_dir']);
-        $this->smarty->setCompileDir($options['compile_dir']);
-    }
+		$this->smarty->setTemplateDir($options['template_dir']);
+		$this->smarty->setCompileDir($options['compile_dir']);
+	}
 
-    /********************************************************************************
-     * Methods
-     *******************************************************************************/
+	/********************************************************************************
+	 * Methods
+	 *******************************************************************************/
 
-    /**
-     * Proxy method to register a plugin to Smarty
-     *
-     * @param string $type plugin type
-     * @param string $tag name of template tag
-     * @param callable $callback PHP callback to register
-     * @param boolean $cacheable if true (default) this function is cachable
-     * @param null $cacheAttr caching attributes if any
-     *
-     * @return self
-     *
-     * @throws SmartyException when the plugin tag is invalid
-     */
-    public function registerPlugin(string $type, string $tag, callable $callback, $cacheable = true, $cacheAttr = null): Smarty
-    {
-        $this->smarty->registerPlugin($type, $tag, $callback, $cacheable, $cacheAttr);
+	/**
+	 * Proxy method to register a plugin to Smarty
+	 *
+	 * @param string $type plugin type
+	 * @param string $tag name of template tag
+	 * @param callable $callback PHP callback to register
+	 * @param boolean $cacheable if true (default) this function is cachable
+	 * @param null $cacheAttr caching attributes if any
+	 *
+	 * @return self
+	 *
+	 * @throws SmartyException when the plugin tag is invalid
+	 */
+	public function registerPlugin(string $type, string $tag, callable $callback, $cacheable = true, $cacheAttr = null): Smarty
+	{
+		$this->smarty->registerPlugin($type, $tag, $callback, $cacheable, $cacheAttr);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * Fetch rendered template
-     *
-     * @param string $template Template pathname relative to templates directory
-     * @param array $data Associative array of template variables
-     *
-     * @return string
-     * @throws SmartyException
-     */
-    public function fetch(string $template, $data = []): string
-    {
-        $data = array_merge($this->defaultVariables, $data??[]);
+	/**
+	 * Fetch rendered template
+	 *
+	 * @param string $template Template pathname relative to templates directory
+	 * @param array $data Associative array of template variables
+	 *
+	 * @return string
+	 * @throws SmartyException
+	 */
+	public function fetch(string $template, $data = []): string
+	{
+		$data = array_merge($this->defaultVariables, $data??[]);
 
-        $this->smarty->assign($data);
+		$this->smarty->assign($data);
 
-        return $this->smarty->fetch($template);
-    }
+		return $this->smarty->fetch($template);
+	}
 
-    /**
-     * Output rendered template
-     *
-     * @param ResponseInterface $response
-     * @param string $template Template pathname relative to templates directory
-     * @param array $data Associative array of template variables
-     * @return ResponseInterface
-     * @throws SmartyException
-     */
-    public function render(ResponseInterface $response, string $template, $data = []): ResponseInterface
-    {
-        $response->getBody()->write($this->fetch($template, $data));
+	/**
+	 * Output rendered template
+	 *
+	 * @param ResponseInterface $response
+	 * @param string $template Template pathname relative to templates directory
+	 * @param array $data Associative array of template variables
+	 * @return ResponseInterface
+	 * @throws SmartyException
+	 */
+	public function render(ResponseInterface $response, string $template, $data = []): ResponseInterface
+	{
+		$response->getBody()->write($this->fetch($template, $data));
 
-        return $response;
-    }
+		return $response;
+	}
 
-    /********************************************************************************
-     * Accessors
-     *******************************************************************************/
+	/********************************************************************************
+	 * Accessors
+	 *******************************************************************************/
 
-    /**
-     * Return Smarty instance
-     *
-     * @return \Smarty
-     */
-    public function getSmarty(): \Smarty\Smarty
-    {
-        return $this->smarty;
-    }
+	/**
+	 * Return Smarty instance
+	 *
+	 * @return \Smarty
+	 */
+	public function getSmarty(): \Smarty\Smarty
+	{
+		return $this->smarty;
+	}
 
-    /********************************************************************************
-     * ArrayAccess interface
-     *******************************************************************************/
+	/********************************************************************************
+	 * ArrayAccess interface
+	 *******************************************************************************/
 
-    /**
-     * Does this collection have a given key?
-     *
-     * @param  string $key The data key
-     *
-     * @return bool
-     */
-    public function offsetExists($key): bool
-    {
-        return array_key_exists($key, $this->defaultVariables);
-    }
+	/**
+	 * Does this collection have a given key?
+	 *
+	 * @param  string $key The data key
+	 *
+	 * @return bool
+	 */
+	public function offsetExists($key): bool
+	{
+		return array_key_exists($key, $this->defaultVariables);
+	}
 
-    /**
-     * Get collection item for key
-     *
-     * @param string $key The data key
-     *
-     * @return mixed The key's value, or the default value
-     */
-    public function offsetGet(mixed $key):mixed
-    {
-        return $this->defaultVariables[$key];
-    }
+	/**
+	 * Get collection item for key
+	 *
+	 * @param string $key The data key
+	 *
+	 * @return mixed The key's value, or the default value
+	 */
+	#[\ReturnTypeWillChange]
+	public function offsetGet($key)
+	{
+		if (!$this->offsetExists($key)) {
+			return null;
+		}
+		return $this->defaultVariables[$key];
+	}
 
-    /**
-     * Set collection item
-     *
-     * @param string $key The data key
-     * @param mixed $value The data value
-     */
-    public function offsetSet(mixed $key, mixed $value): void
-    {
-        $this->defaultVariables[$key] = $value;
-    }
+	/**
+	 * Set collection item
+	 *
+	 * @param string $key The data key
+	 * @param mixed $value The data value
+	 */
+	public function offsetSet($key, $value): void
+	{
+		$this->defaultVariables[$key] = $value;
+	}
 
-    /**
-     * Remove item from collection
-     *
-     * @param string $key The data key
-     */
-    public function offsetUnset(mixed $key): void
-    {
-        unset($this->defaultVariables[$key]);
-    }
+	/**
+	 * Remove item from collection
+	 *
+	 * @param string $key The data key
+	 */
+	public function offsetUnset($key): void
+	{
+		unset($this->defaultVariables[$key]);
+	}
 
-    /********************************************************************************
-     * Countable interface
-     *******************************************************************************/
+	/********************************************************************************
+	 * Countable interface
+	 *******************************************************************************/
 
-    /**
-     * Get number of items in collection
-     *
-     * @return int
-     */
-    public function count(): int
-    {
-        return count($this->defaultVariables);
-    }
+	/**
+	 * Get number of items in collection
+	 *
+	 * @return int
+	 */
+	public function count(): int
+	{
+		return count($this->defaultVariables);
+	}
 
-    /********************************************************************************
-     * IteratorAggregate interface
-     *******************************************************************************/
+	/********************************************************************************
+	 * IteratorAggregate interface
+	 *******************************************************************************/
 
-    /**
-     * Get collection iterator
-     *
-     * @return ArrayIterator
-     */
-    public function getIterator(): ArrayIterator
-    {
-        return new ArrayIterator($this->defaultVariables);
-    }
+	/**
+	 * Get collection iterator
+	 *
+	 * @return ArrayIterator
+	 */
+	public function getIterator(): ArrayIterator
+	{
+		return new ArrayIterator($this->defaultVariables);
+	}
 }
